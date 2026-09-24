f9b8b0ef25fc6c1acf9f4035b140d339c8d4dde7
Hasan Yıldırım <leonscripting@gmail.com>
2026-09-24 13:56:10 +0300

    Expose RugScale internals to tests and audit tools

---
+++ src/RugScale.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("RugScale.Core.Tests")]
+[assembly: InternalsVisibleTo("RugScale.CurveScaleAudit")]
+[assembly: InternalsVisibleTo("RugScale.RugScaleAudit")]
