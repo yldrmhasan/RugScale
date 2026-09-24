3b87fcde4456881dbe2ec2984af049759bda8182
Hasan Yıldırım <leonscripting@gmail.com>
2026-09-25 02:11:13 +0300

    Prefer sub-pixel Through Points fitting for broad filled arches

--- src/RugScale.Core/Drawing/RugScale/CurveFillRibbonArcRefiner.cs
+++ src/RugScale.Core/Drawing/RugScale/CurveFillRibbonArcRefiner.cs
@@ -181,32 +181,12 @@ internal static class CurveFillRibbonArcRefiner
             // contains the designer's constant/slowly-varying band thickness.
             ElegantArcFit fit;
 
-            if (CurveFillRibbonToolFitter.TryFit(
-                    model,
-                    out var toolFit,
-                    out var toolStyle))
+            if (broadSparseArch)
             {
-                fit =
-                    toolFit;
-                curveToolFits++;
-                curveToolRoundnessSum +=
-                    toolStyle.Roundness;
-
-                switch (toolStyle.Type)
-                {
-                    case CurveType.SplineThroughPoints:
-                        curveToolThroughPointsFits++;
-                        break;
-                    case CurveType.Spline:
-                        curveToolSplineFits++;
-                        break;
-                    case CurveType.Bezier:
-                        curveToolBezierFits++;
-                        break;
-                }
-            }
-            else if (broadSparseArch)
-            {
+                // Broad filled arches are recovered from a sub-pixel medial path. Try the
+                // geometric Through-Points inverse model BEFORE the ordinary 1x1 Curve learner:
+                // the latter rounds the re-centred medial samples to integer cells and can
+                // accidentally reintroduce the source staircase phase we just removed.
                 geometricThroughAttempts++;
 
                 if (CurveFillRibbonThroughPointsFitter.TryFit(
@@ -219,6 +199,30 @@ internal static class CurveFillRibbonArcRefiner
                     geometricThroughFits++;
                     geometricThroughRoundnessSum +=
                         throughDiagnostics.Roundness;
+                }
+                else if (CurveFillRibbonToolFitter.TryFit(
+                             model,
+                             out var toolFit,
+                             out var toolStyle))
+                {
+                    fit =
+                        toolFit;
+                    curveToolFits++;
+                    curveToolRoundnessSum +=
+                        toolStyle.Roundness;
+
+                    switch (toolStyle.Type)
+                    {
+                        case CurveType.SplineThroughPoints:
+                            curveToolThroughPointsFits++;
+                            break;
+                        case CurveType.Spline:
+                            curveToolSplineFits++;
+                            break;
+                        case CurveType.Bezier:
+                            curveToolBezierFits++;
+                            break;
+                    }
                 }
                 else
                 {
@@ -294,6 +298,30 @@ internal static class CurveFillRibbonArcRefiner
                     Math.Max(
                         maxGeometricThroughP95Deviation,
                         throughDiagnostics.Percentile95Deviation);
+            }
+            else if (CurveFillRibbonToolFitter.TryFit(
+                         model,
+                         out var toolFit,
+                         out var toolStyle))
+            {
+                fit =
+                    toolFit;
+                curveToolFits++;
+                curveToolRoundnessSum +=
+                    toolStyle.Roundness;
+
+                switch (toolStyle.Type)
+                {
+                    case CurveType.SplineThroughPoints:
+                        curveToolThroughPointsFits++;
+                        break;
+                    case CurveType.Spline:
+                        curveToolSplineFits++;
+                        break;
+                    case CurveType.Bezier:
+                        curveToolBezierFits++;
+                        break;
+                }
             }
             else
             {
