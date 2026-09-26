1dd36c8b2e79c4b82df8bf4dbadbae752e1fb02d
Hasan Yıldırım <leonscripting@gmail.com>
2026-09-26 04:24:19 +0300

    Restore outlined ribbon raster after coverage experiment

--- src/RugScale.Core/Drawing/RugScale/CurveFillOutlinedRibbonRasterizer.cs
+++ src/RugScale.Core/Drawing/RugScale/CurveFillOutlinedRibbonRasterizer.cs
@@ -65,7 +65,7 @@ internal static class CurveFillOutlinedRibbonRasterizer
             return false;
 
         var targetMask =
-            LeafPetalArcRasterizer.RasterizePolygonBalanced(
+            LeafPetalArcRasterizer.RasterizePolygon(
                 polygon,
                 destination.Width,
                 destination.Height);
@@ -92,7 +92,7 @@ internal static class CurveFillOutlinedRibbonRasterizer
                 scaleX,
                 scaleY);
         var compoundMask =
-            LeafPetalArcRasterizer.RasterizePolygonBalanced(
+            LeafPetalArcRasterizer.RasterizePolygon(
                 compoundPolygon,
                 destination.Width,
                 destination.Height);
