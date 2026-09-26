03a77a23f7c25e05d0220c47898002225e9e3589
Hasan Yıldırım <leonscripting@gmail.com>
2026-09-26 19:20:34 +0300

    Report compound selection configuration in curve audit

--- tools/RugScale.CurveScaleAudit/Program.cs
+++ tools/RugScale.CurveScaleAudit/Program.cs
@@ -1144,7 +1144,9 @@ internal static class Program
                     $"mainArc={stage.MainArcExtracted}/{stage.MainArcReason} " +
                     $"{stage.MainArcStart}-{stage.MainArcEnd} keep={stage.MainArcKeptFraction:0.000}, " +
                     $"compound={stage.CompoundAttempted}/{stage.CompoundReason} " +
-                    $"p95={stage.CompoundP95Deviation:0.000} max={stage.CompoundMaximumDeviation:0.000}, " +
+                    $"p95={stage.CompoundP95Deviation:0.000} max={stage.CompoundMaximumDeviation:0.000} " +
+                    $"rough={stage.CompoundSelectedRoughness:0.000000} " +
+                    $"cfg={stage.CompoundSelectedAnchors}a/{stage.CompoundSelectedSmoothingPasses}s, " +
                     $"fit={stage.FitKind}/{stage.CurveFamily}, r={stage.Roundness:0.000}, " +
                     $"safe={stage.FitSafe}, dev={stage.MaximumDeviation:0.000}, rough={stage.SelectedSmoothness:0.000000}, " +
                     $"altThrough={stage.AlternativeThroughSafe} r={stage.AlternativeThroughRoundness:0.000} " +
@@ -1739,7 +1741,7 @@ internal static class Program
             new StringBuilder();
 
         sb.AppendLine(
-            "color,min_x,min_y,max_x,max_y,area,elongation,boundary_ratio,bounding_fill,broad_sparse_arch,prefilter,centerline,skeleton_pixels,endpoints,principal_path_pixels,path_coverage,designer_ribbon,ribbon_shape_reason,ribbon_mean_width,ribbon_width_cv,ribbon_terminal_ratio,ribbon_max_bend,ribbon_required_bend,symmetry_recovered,symmetry_axis,mirror_agreement,symmetry_mean_shift,symmetry_max_shift,mirror_source_recovered,mirror_source_reason,mirror_source_agreement,mirror_source_mean_shift,mirror_source_max_shift,main_arc_extracted,main_arc_reason,main_arc_start,main_arc_end,main_arc_kept_fraction,compound_attempted,compound_reason,compound_p95_deviation,compound_max_deviation,fit_kind,curve_family,roundness,fit_safe,max_deviation,curvature_flips,selected_smoothness,alt_through_safe,alt_through_smoothness,alt_through_max_deviation,alt_through_p95_deviation,alt_through_roundness,width_reg_applied,width_source_cv,width_terminal_ratio,width_variation_before,width_variation_after,width_max_shift,accepted,status");
+            "color,min_x,min_y,max_x,max_y,area,elongation,boundary_ratio,bounding_fill,broad_sparse_arch,prefilter,centerline,skeleton_pixels,endpoints,principal_path_pixels,path_coverage,designer_ribbon,ribbon_shape_reason,ribbon_mean_width,ribbon_width_cv,ribbon_terminal_ratio,ribbon_max_bend,ribbon_required_bend,symmetry_recovered,symmetry_axis,mirror_agreement,symmetry_mean_shift,symmetry_max_shift,mirror_source_recovered,mirror_source_reason,mirror_source_agreement,mirror_source_mean_shift,mirror_source_max_shift,main_arc_extracted,main_arc_reason,main_arc_start,main_arc_end,main_arc_kept_fraction,compound_attempted,compound_reason,compound_p95_deviation,compound_max_deviation,compound_selected_roughness,compound_selected_anchors,compound_selected_smoothing_passes,fit_kind,curve_family,roundness,fit_safe,max_deviation,curvature_flips,selected_smoothness,alt_through_safe,alt_through_smoothness,alt_through_max_deviation,alt_through_p95_deviation,alt_through_roundness,width_reg_applied,width_source_cv,width_terminal_ratio,width_variation_before,width_variation_after,width_max_shift,accepted,status");
 
         foreach (var stage in stages)
         {
@@ -1785,6 +1787,9 @@ internal static class Program
             sb.Append(stage.CompoundReason).Append(',');
             sb.Append(stage.CompoundP95Deviation.ToString("0.000000", CultureInfo.InvariantCulture)).Append(',');
             sb.Append(stage.CompoundMaximumDeviation.ToString("0.000000", CultureInfo.InvariantCulture)).Append(',');
+            sb.Append(stage.CompoundSelectedRoughness.ToString("0.000000", CultureInfo.InvariantCulture)).Append(',');
+            sb.Append(stage.CompoundSelectedAnchors).Append(',');
+            sb.Append(stage.CompoundSelectedSmoothingPasses).Append(',');
             sb.Append(stage.FitKind).Append(',');
             sb.Append(stage.CurveFamily).Append(',');
             sb.Append(stage.Roundness.ToString("0.000000", CultureInfo.InvariantCulture)).Append(',');
